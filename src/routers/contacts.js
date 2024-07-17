9811819e537341a13697e55c7f374403a9d7d4c3
Vitaliia1109 <vitusechka@ukr.net>
2024-07-17 18:10:34 +0100

    try-1

---
+++ src/routers/contacts.js
@@ -0,0 +1,39 @@
+import { Router } from 'express';
+
+import {
+  getContactsController,
+  getContactByIdController,
+  createContactController,
+  patchContactController,
+  deleteСontactController,
+} from '../controllers/contacts.js';
+
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+
+import { isValidId } from '../middlewares/isValidId.js';
+
+const router = Router();
+
+router.get('/contacts', ctrlWrapper(getContactsController));
+
+router.get(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(getContactByIdController),
+);
+
+router.post('/contacts', ctrlWrapper(createContactController));
+
+router.patch(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(patchContactController),
+);
+
+router.delete(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(deleteСontactController),
+);
+
+export default router;
